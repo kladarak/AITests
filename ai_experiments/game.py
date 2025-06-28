e0a67660df30fad427e973767c1da20caddcc5a9
kladarak <scottjenkins88@gmail.com>
2025-06-28 23:21:22 +0200

    Introduced Editor Mode
    Prompt: I would like to introduce an editor mode vs game mode. The game should load the data as normal, but the objects should not be interactable by default. Place a checkbox in the top left corner which should be used to enable editor mode. This should set a flag which enables editing of the object position and sizes.
    
    Then:
    There is a bug in the code: MOUSEBUTTONDOWN events are not passed down into the editor when editor mode is enabled.

--- ai_experiments/game.py
+++ ai_experiments/game.py
@@ -4,6 +4,16 @@ from .scene_data_manager import SceneDataManager
 from .scene import Scene
 from .scene_editor import SceneEditor
 
+def draw_checkbox(surface, checked, font):
+    box_rect = pygame.Rect(10, 10, 24, 24)
+    pygame.draw.rect(surface, (255, 255, 255), box_rect, 2)
+    if checked:
+        pygame.draw.line(surface, (0, 255, 0), (14, 22), (22, 14), 3)
+        pygame.draw.line(surface, (0, 255, 0), (14, 14), (22, 22), 3)
+    label = font.render("Editor Mode", True, (255, 255, 255))
+    surface.blit(label, (40, 10))
+    return box_rect
+
 def run_game():
     pygame.init()
     assets_dir = Path(__file__).parent / "assets"
@@ -15,16 +25,25 @@ def run_game():
     pygame.display.set_caption("Point and Click Demo")
     font = pygame.font.SysFont(None, 24)
 
+    editor_mode = False
+
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            editor.handle_event(event)
+            checkbox_rect = pygame.Rect(10, 10, 24, 24)
+            if event.type == pygame.MOUSEBUTTONDOWN and checkbox_rect.collidepoint(event.pos):
+                editor_mode = not editor_mode
+            elif editor_mode:
+                editor.handle_event(event)
 
-        editor.update()
+        if editor_mode:
+            editor.update()
         scene.draw(screen)
-        editor.draw_status(screen, font)
+        draw_checkbox(screen, editor_mode, font)
+        if editor_mode:
+            editor.draw_status(screen, font)
         pygame.display.flip()
 
     pygame.quit()
