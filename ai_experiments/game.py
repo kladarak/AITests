362ef34bedbb7ce9d724db6e9aa4f3f58462022c
kladarak <scottjenkins88@gmail.com>
2025-06-28 23:02:49 +0200

    Broke up function into classes
    Prompt: This function is starting to get quite big. Please break up the logic into separate classes and functions following SOLID principles.

--- ai_experiments/game.py
+++ ai_experiments/game.py
@@ -2,119 +2,155 @@ import pygame
 from pathlib import Path
 import json
 
+class SceneDataManager:
+    def __init__(self, data_file):
+        self.data_file = data_file
+        self.data = self.load()
+
+    def load(self):
+        if self.data_file.exists():
+            with open(self.data_file, "r") as f:
+                return json.load(f)
+        return {}
+
+    def get_object_data(self, key, default):
+        return self.data.get(key, default)
+
+    def update_object(self, key, rect):
+        self.data[key] = {
+            "x": rect.x,
+            "y": rect.y,
+            "width": rect.width,
+            "height": rect.height
+        }
+        self.save()
+
+    def save(self):
+        with open(self.data_file, "w") as f:
+            json.dump(self.data, f, indent=4)
+
+class SceneObject:
+    def __init__(self, key, img_path, obj_data):
+        self.key = key
+        self.img_path = img_path
+        self.image = pygame.image.load(str(img_path))
+        self.rect = pygame.Rect(
+            obj_data["x"], obj_data["y"], obj_data["width"], obj_data["height"]
+        )
+        self.update_image()
+
+    def update_image(self):
+        self.image = pygame.image.load(str(self.img_path))
+        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
+
+class Scene:
+    def __init__(self, assets_dir, data_manager):
+        self.assets_dir = assets_dir
+        self.data_manager = data_manager
+        self.objects = self.load_objects()
+        self.background = pygame.image.load(str(self.assets_dir / "astronomy_tower.jpeg"))
+        self.bg_width, self.bg_height = self.background.get_size()
+
+    def load_objects(self):
+        image_paths = list((self.assets_dir / "items").glob("*.png")) + list((self.assets_dir / "potions").glob("*.png"))
+        objects = {}
+        for img_path in image_paths:
+            key = img_path.stem
+            obj_data = self.data_manager.get_object_data(key, {"x": 0, "y": 0, "width": 100, "height": 100})
+            objects[key] = SceneObject(key, img_path, obj_data)
+        return objects
+
+    def draw(self, surface):
+        surface.blit(self.background, (0, 0))
+        for obj in self.objects.values():
+            obj.draw(surface)
+
+    def get_object_at_pos(self, pos):
+        for key, obj in self.objects.items():
+            if obj.rect.collidepoint(pos):
+                return key, obj
+        return None, None
+
+    def update_object(self, key):
+        obj = self.objects[key]
+        obj.update_image()
+        self.data_manager.update_object(key, obj.rect)
+
+class SceneEditor:
+    def __init__(self, scene):
+        self.scene = scene
+        self.dragging_key = None
+        self.offset_x = 0
+        self.offset_y = 0
+        self.resized = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            key, obj = self.scene.get_object_at_pos(event.pos)
+            if obj:
+                self.dragging_key = key
+                mouse_x, mouse_y = event.pos
+                self.offset_x = obj.rect.x - mouse_x
+                self.offset_y = obj.rect.y - mouse_y
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if self.dragging_key:
+                self.scene.update_object(self.dragging_key)
+            self.dragging_key = None
+        elif event.type == pygame.MOUSEMOTION and self.dragging_key:
+            mouse_x, mouse_y = event.pos
+            obj = self.scene.objects[self.dragging_key]
+            obj.rect.x = mouse_x + self.offset_x
+            obj.rect.y = mouse_y + self.offset_y
+        elif event.type == pygame.KEYDOWN and self.dragging_key:
+            obj = self.scene.objects[self.dragging_key]
+            if event.key == pygame.K_UP:
+                obj.rect.width += 5
+                obj.rect.height += 5
+                self.resized = True
+            elif event.key == pygame.K_DOWN and obj.rect.width > 10 and obj.rect.height > 10:
+                obj.rect.width -= 5
+                obj.rect.height -= 5
+                self.resized = True
+
+    def update(self):
+        if self.resized and self.dragging_key:
+            obj = self.scene.objects[self.dragging_key]
+            obj.update_image()
+            self.scene.update_object(self.dragging_key)
+            self.resized = False
+
+    def draw_status(self, surface, font):
+        if self.dragging_key:
+            obj = self.scene.objects[self.dragging_key]
+            pos_text = f"{self.dragging_key}: ({obj.rect.x}, {obj.rect.y}) {obj.rect.width}x{obj.rect.height}"
+            text_surf = font.render(pos_text, True, (255, 255, 255))
+            text_rect = text_surf.get_rect(topright=(self.scene.bg_width - 10, 10))
+            surface.blit(text_surf, text_rect)
+
 def run_game():
     pygame.init()
-
-    # Set up asset paths using pathlib
     assets_dir = Path(__file__).parent / "assets"
     data_file = assets_dir / "scene_data.json"
-
-    # Find all PNG files in assets/items and assets/potions
-    image_paths = list((assets_dir / "items").glob("*.png")) + list((assets_dir / "potions").glob("*.png"))
-
-    # Load scene data from JSON
-    if data_file.exists():
-        with open(data_file, "r") as f:
-            scene_data = json.load(f)
-    else:
-        scene_data = {}
-
-    # Load background
-    background_img = pygame.image.load(str(assets_dir / "astronomy_tower.jpeg"))
-    bg_width, bg_height = background_img.get_size()
-    screen = pygame.display.set_mode((bg_width, bg_height))
+    data_manager = SceneDataManager(data_file)
+    scene = Scene(assets_dir, data_manager)
+    editor = SceneEditor(scene)
+    screen = pygame.display.set_mode((scene.bg_width, scene.bg_height))
     pygame.display.set_caption("Point and Click Demo")
-
-    # Load all images and their rects
-    objects = {}
-    for img_path in image_paths:
-        key = img_path.stem  # filename without extension
-        # Get data or default
-        obj_data = scene_data.get(key, {"x": 0, "y": 0, "width": 100, "height": 100})
-        img = pygame.image.load(str(img_path))
-        img = pygame.transform.scale(img, (obj_data["width"], obj_data["height"]))
-        rect = img.get_rect(topleft=(obj_data["x"], obj_data["y"]))
-        objects[key] = {"img_path": img_path, "img": img, "rect": rect}
-
     font = pygame.font.SysFont(None, 24)
-
-    dragging_key = None
-    offset_x = 0
-    offset_y = 0
-    resized = False
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                for key, obj in objects.items():
-                    if obj["rect"].collidepoint(event.pos):
-                        dragging_key = key
-                        mouse_x, mouse_y = event.pos
-                        offset_x = obj["rect"].x - mouse_x
-                        offset_y = obj["rect"].y - mouse_y
-                        break
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if dragging_key:
-                    # Save new position to JSON on drop
-                    obj = objects[dragging_key]
-                    scene_data[dragging_key] = {
-                        "x": obj["rect"].x,
-                        "y": obj["rect"].y,
-                        "width": obj["rect"].width,
-                        "height": obj["rect"].height
-                    }
-                    with open(data_file, "w") as f:
-                        json.dump(scene_data, f, indent=4)
-                dragging_key = None
-            elif event.type == pygame.MOUSEMOTION and dragging_key:
-                mouse_x, mouse_y = event.pos
-                obj = objects[dragging_key]
-                obj["rect"].x = mouse_x + offset_x
-                obj["rect"].y = mouse_y + offset_y
-            elif event.type == pygame.KEYDOWN and dragging_key:
-                obj = objects[dragging_key]
-                # Resize with up/down arrows for quick scene setup
-                if event.key == pygame.K_UP:
-                    obj["rect"].width += 5
-                    obj["rect"].height += 5
-                    resized = True
-                elif event.key == pygame.K_DOWN and obj["rect"].width > 10 and obj["rect"].height > 10:
-                    obj["rect"].width -= 5
-                    obj["rect"].height -= 5
-                    resized = True
+            editor.handle_event(event)
 
-        # If resized, update image and save to JSON
-        if resized and dragging_key:
-            obj = objects[dragging_key]
-            obj["img"] = pygame.image.load(str(obj["img_path"]))
-            obj["img"] = pygame.transform.scale(obj["img"], (obj["rect"].width, obj["rect"].height))
-            scene_data[dragging_key] = {
-                "x": obj["rect"].x,
-                "y": obj["rect"].y,
-                "width": obj["rect"].width,
-                "height": obj["rect"].height
-            }
-            with open(data_file, "w") as f:
-                json.dump(scene_data, f, indent=4)
-            resized = False
-
-        # Draw background
-        screen.blit(background_img, (0, 0))
-        # Draw all objects
-        for key, obj in objects.items():
-            screen.blit(obj["img"], obj["rect"].topleft)
-
-        # Draw position and size of dragged object in top right corner
-        if dragging_key:
-            obj = objects[dragging_key]
-            pos_text = f"{dragging_key}: ({obj['rect'].x}, {obj['rect'].y}) {obj['rect'].width}x{obj['rect'].height}"
-            text_surf = font.render(pos_text, True, (255, 255, 255))
-            text_rect = text_surf.get_rect(topright=(bg_width - 10, 10))
-            screen.blit(text_surf, text_rect)
-
+        editor.update()
+        scene.draw(screen)
+        editor.draw_status(screen, font)
         pygame.display.flip()
 
     pygame.quit()
